8b7e909a9fa84c5792e93ab35a4b658593cc0fdd
tfranssen <54217714+tfranssen@users.noreply.github.com>
2020-09-15 16:31:07 +0200

    Update http_test_post.ino

--- http_test_post/http_test_post.ino
+++ http_test_post/http_test_post.ino
@@ -31,6 +31,8 @@
 */
 
 #include <Sodaq_R4X.h>
+#include <Sodaq_wdt.h>
+#include <Sodaq_LSM303AGR.h>
 #include "sha256.h"
 
 
@@ -48,17 +50,33 @@
 #define HTTP_QUERY       "/ingestion/m2m/senml/v1"
 
 static Sodaq_R4X r4x;
+static Sodaq_LSM303AGR AccMeter;
 static Sodaq_SARA_R4XX_OnOff saraR4xxOnOff;
 static bool isReady;
+static bool isOff;
 
 #ifndef NBIOT_BANDMASK
 #define NBIOT_BANDMASK BAND_MASK_UNCHANGED
 #endif
 
 #define MAX_ALLOWED_BUFFER 512
+#define STARTUP_DELAY 5000
+
+unsigned long previousMillis = 0;
+const long interval = 60000;
 
 void setup()
 {
+  sodaq_wdt_safe_delay(STARTUP_DELAY);
+
+  Wire.begin();
+
+  AccMeter.rebootAccelerometer();
+  delay(1000);
+
+  // Enable the Accelerometer
+  AccMeter.enableAccelerometer();
+
   while ((!CONSOLE_STREAM) && (millis() < 10000)) {
     // Wait max 10 sec for the CONSOLE_STREAM to open
   }
@@ -67,6 +85,11 @@ void setup()
   MODEM_STREAM.begin(r4x.getDefaultBaudrate());
 
   r4x.setDiag(CONSOLE_STREAM);
+
+}
+
+void loop()
+{
   r4x.init(&saraR4xxOnOff, MODEM_STREAM);
 
   isReady = r4x.connect(CURRENT_APN, CURRENT_URAT, CURRENT_MNO_PROFILE, CURRENT_OPERATOR, BAND_MASK_UNCHANGED, NBIOT_BANDMASK);
@@ -76,63 +99,75 @@ void setup()
   if (isReady) {
     postHTTP();
   }
-}
-
-void loop()
-{
-  if (CONSOLE_STREAM.available()) {
-    int i = CONSOLE_STREAM.read();
-    CONSOLE_STREAM.write(i);
-    MODEM_STREAM.write(i);
-  }
-
-  if (MODEM_STREAM.available()) {
-    CONSOLE_STREAM.write(MODEM_STREAM.read());
-  }
-}
-
-void postHTTP()
-{
-  char buffer[2048];
-  //Prepare send buffer
-  String message = "[{\"bn\":\"urn:dev:IMEI:356726104408422:\"},{\"n\": \"temperature\", \"v\": 20.5, \"u\": \"Cel\"}]"; 
-  char sendbuffer[message.length() + 1];
-  message.toCharArray(sendbuffer, message.length() + 1);  
-  CONSOLE_STREAM.print("Body:");
-  CONSOLE_STREAM.print(message);  
-  CONSOLE_STREAM.print("\n");
-
-  //Prepare messageToken
-  String sharedSecret = "72J3wD2bd49jnMI8S31j2L3vZldH3rSS";
-  CONSOLE_STREAM.print("sharedSecret:\n");
-  CONSOLE_STREAM.print(sharedSecret);  
-  CONSOLE_STREAM.print("\n"); 
+  isOff = r4x.off();
+  CONSOLE_STREAM.println(isOff ? "Modem off" : "Power off failed");
+  sodaq_wdt_safe_delay(60000);
   
-  String toBeHashed = message+"\0"+sharedSecret;
-  CONSOLE_STREAM.print("Sring to hash:\n");
-  CONSOLE_STREAM.print(toBeHashed);  
-  CONSOLE_STREAM.print("\n");  
+}
+
+int8_t getBoardTemperature()
+{
+  int8_t temp = AccMeter.getTemperature();
+  return temp;
+}
+
+String hashMessage(String toBeHashed) {
   uint8_t *hash;
   Sha256.init();
   Sha256.print(toBeHashed);
   uint8_t * result = Sha256.result();
   String token = "";
-  CONSOLE_STREAM.print("Hash:\n");
-
   for (int i = 0; i < 32; i++) {
-          token = token + "0123456789abcdef"[result[i] >> 4];
-          token = token + "0123456789abcdef"[result[i] & 0xf];
+    token = token + "0123456789abcdef"[result[i] >> 4];
+    token = token + "0123456789abcdef"[result[i] & 0xf];
   }
+
+  return token;
+}
+
+void postHTTP()
+{
+
+  char buffer[2048] = "/0";
+
+  //Prepare message
+  String message = "[{\"bn\":\"urn:dev:IMEI:356726104408422:\"},{\"n\": \"temperature\", \"v\":" + String(getBoardTemperature()) + ", \"u\": \"Cel\"}]";
+
+  //Convert message string to Char Array
+  char sendbuffer[message.length() + 1];
+  message.toCharArray(sendbuffer, message.length() + 1);
+  CONSOLE_STREAM.print("Body:");
+  CONSOLE_STREAM.print(message);
+  CONSOLE_STREAM.print("\n");
+
+  //Prepare messageToken
+  String sharedSecret = "72J3wD2bd49jnMI8S31j2L3vZldH3rSS";
+  CONSOLE_STREAM.print("sharedSecret:\n");
+  CONSOLE_STREAM.print(sharedSecret);
+  CONSOLE_STREAM.print("\n");
+
+  String toBeHashed = message + "\0" + sharedSecret;
+  CONSOLE_STREAM.print("String to hash:\n");
+  CONSOLE_STREAM.print(toBeHashed);
+  CONSOLE_STREAM.print("\n");
+
+  //Hash message
+  String token = hashMessage(toBeHashed);
+
+  CONSOLE_STREAM.print("Hash:\n");
   CONSOLE_STREAM.print(token);
-  CONSOLE_STREAM.print("\n");  
-  
+  CONSOLE_STREAM.print("\n");
+
+  //Convert token string to Char array
   char tokenBuffer[token.length() + 1];
-  token.toCharArray(tokenBuffer, token.length() + 1);   
-  
+  token.toCharArray(tokenBuffer, token.length() + 1);
+
+  CONSOLE_STREAM.print("Sending message \n");
+
   r4x.httpClear();
   r4x.httpSetCustomHeader(0, "content-type", "application/json");
-  r4x.httpSetCustomHeader(1 , "Things-Message-Token", tokenBuffer);  
-  r4x.httpSetCustomHeader(2 , "host", "m.m");  
+  r4x.httpSetCustomHeader(1 , "Things-Message-Token", tokenBuffer);
+  r4x.httpSetCustomHeader(2 , "host", "m.m");
   char i =  r4x.httpRequest(HTTP_HOST, HTTP_PORT, HTTP_QUERY,
                             HttpRequestTypes::POST,
                             buffer,   sizeof(buffer),
